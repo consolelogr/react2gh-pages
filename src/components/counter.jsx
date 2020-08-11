9162342a07cc35746d3d050fb4b85ddd8d9ad9d6
Consolelogr <consolelogger@ftml.net>
2020-08-12 01:51:24 +0300

    testing2

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,39 +3,35 @@ import './styyli.css';
 
 class Counter extends Component {
   state = {
-    count: 1,
+    value: this.props.value,
+    count: 0
   };
 
   
-  handleIncrement = (product)  => {
-    console.log(product);
+  handleIncrement = product => {
     this.setState( {count: this.state.count+1} ) ;
   }
 
-doHandleIncrement = (product) => {
-  
-  this.handleIncrement({ id: 1});
-}
   
 render() {   
     return ( 
         <div className="wrappi">
-        <span className={this.styyliChangee}>{this.formatCount()}</span> 
-        <button onClick={this.doHandleIncrement}>BUTTON</button>
+        <span className={this.styyliChangee()}> {this.formatCount()}</span> 
+        <button onClick={ () => {this.handleIncrement()}} > BUTTON</button>
       </div> 
     );   
   }
 
   styyliChangee() {   
-    let classes = "spanni spanni";
+    let classes = "spanni";
     classes += this.state.count === 0 ? "" : "Tiukka";
-    return classes;
+    return classes ;
   }
 
   formatCount() {
     
     const {count} = this.state;
-      return count === 0 ? 'zero' : count;
+      return count === 0 ? '0' : count;
   }
 }
  
